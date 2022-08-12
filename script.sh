6362b44ad4e0bbbb10d871601c486312cc8d0524
mpadge <mark.padgham@email.com>
2022-08-12 14:31:42 +0200

    Initial Site Build (Fri 12 Aug 14:31:42 CEST 2022)

--- script.sh
+++ script.sh
@@ -12,14 +12,11 @@ yarn build
 git checkout main
 
 echo -e "\033[0;32mDeleting old site...\033[0m"
-rm -r assets/ blog/
+rm -r assets/
 
 echo -e "\033[0;32mTransferring new contents...\033[0m"
-mkdir assets blog
+mkdir assets
 mv dist/index.html .
-mv dist/blog.html .
-mv dist/privacy.html .
-mv dist/blog/*.html blog/.
 mv dist/assets/* assets/.
 rm -r dist
 
@@ -28,7 +25,7 @@ rm -r dist
 sed -i '1d' index.html
 
 echo -e "\033[0;32mUpdating git...\033[0m"
-git add index.html blog.html privacy.html blog/* assets/*
+git add index.html assets/*
 git add -u
 git st
 git commit -am "New Blog Build (`date`)"
